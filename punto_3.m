6f461bdc3cd2aa9a27b3df2ab5b82f651b7ad114
unknown <s293527@studenti.polito.it>
2024-11-23 11:59:47 +0100

    inizio punto 3 andre tere dacci la tua matricola ziopera

---
+++ punto_3.m
@@ -0,0 +1,14 @@
+%%%%%%%% FIRST POINT %%%%%%%%
+
+seed = min([341965, 343316]);
+
+rng(seed);
+
+%%%%%%%% SECOND POINT %%%%%%%%
+
+d = 3;    % alternative: 3,4,5
+
+n = 10^d;
+
+%%%%%%%% THIRD POINT %%%%%%%%
+
