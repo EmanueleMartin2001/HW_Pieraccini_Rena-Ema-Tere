c39ae2cfc7db3ed739c94e95d8bcc3904ee3d7ae
unknown <s293527@studenti.polito.it>
2024-11-25 21:52:15 +0100

    some fix

--- punto_3.m
+++ punto_3.m
@@ -18,7 +18,7 @@ rng(seed);
 
 d = 3;    % alternative: 3,4,5
 
-n = 15;     % n = 10^d
+n = 10^d
 
 %% for the first function we choose the Chainedd Rosenbrock function
 
@@ -28,11 +28,11 @@ f1 = @(x) sum(100 * (x(1:end-1).^2 -x(2:end)).^2 + (x(1:end-1)-1).^2);
 
 % gradient construction:
 
-f1_grad_comp = cell(1,n);
+f1_grad_comp = cell(n,1);
 
-f1_grad_comp{1} = @(x) 400*x(1)^3 -400*x(1)*x(2) + 2*x(1) - 1;
+f1_grad_comp{1} = @(x) 400*x(1)^3 -400*x(1)*x(2) + 2*x(1) - 2;
 for i = 2:1:(n-1)
-    f1_grad_comp{i} = @(x) 400*x(i)^3 -200*x(i-1)^2 - 400*x(i+1)*x(i) + 202*x(i);
+    f1_grad_comp{i} = @(x) 400*x(i)^3 -200*x(i-1)^2 - 400*x(i+1)*x(i) + 202*x(i) -2;
 end
 f1_grad_comp{n} = @(x) -200*(x(n-1)^2 - x(n));
 
@@ -52,7 +52,7 @@ end
 A1{n} = @(x) 200;
 B1{n} = @(x) 0; %forced by spdiags
 
-Hessian_f1 = @(x) spdiags([cell2mat(cellfun(@(B1) B1(x), B1, 'UniformOutput', false)), ...
+Hessf1 = @(x) spdiags([cell2mat(cellfun(@(B1) B1(x), B1, 'UniformOutput', false)), ...
                            cell2mat(cellfun(@(A1) A1(x), A1, 'UniformOutput', false)), ...
                            cell2mat(cellfun(@(B1) B1(x), B1, 'UniformOutput', false))], ...
                           [-1, 0, 1], n, n);
@@ -87,5 +87,20 @@ X_f1 = X_f1 + error;    % each column of this vector represent a starting point
 %%%%%%%% END THIRD POINT %%%%%%%%
 
 rho = 0.5;
-c = 10^-4;
+c = 1e-4;
 
+kmax = 10;
+tolgrad = 1e-5;
+btmax = 1;
+
+% calling the method:
+
+[xk, fk, gradfk_norm, k, xseq, btseq] = ...
+    Modified_Newton_method(X_f1(:,1), f1, gradf1, Hessf1, ...
+    kmax, tolgrad, c, rho, btmax)
+
+[xk1, fk1, gradfk_norm1, k1, xseq1, btseq1] = ...
+    newton_bcktrck(X_f1(:,1), f1, gradf1, Hessf1, ...
+    kmax, tolgrad, c, rho, btmax)
+
+norm(xk-xk1)
